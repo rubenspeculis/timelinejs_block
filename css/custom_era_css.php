7767b2ae729345f1e28705b1b0716d9a07a86543
Rubens Peculis <rubens.peculis@mdba.gov.au>
2013-07-18 12:20:27 +1000

    Increased Opacity

--- css/custom_era_css.php
+++ css/custom_era_css.php
@@ -12,7 +12,8 @@ $css = '';
 if(isset($era)) {
   foreach($era as $class => $color) {
     $css .= '.block-timelinejs-block .' . $class . ' { color: ' . $color . ' !Important; }'."\n";
-    $css .= '.block-timelinejs-block .' . $class . ' div { background: ' . $color . ' !Important; }'."\n";
+    $css .= '.block-timelinejs-block .' . $class . ' div { background: ' . $color . ' !Important;';
+    $css .= 'filter: alpha(opacity=30) !Important; khtml-opacity: 0.3 !Important; opacity: 0.3 !Important}'."\n";
   }
 }
 

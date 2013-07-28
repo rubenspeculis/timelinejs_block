058ec6e96be14cabdbe9bc3de20c2e5b50e7caea
Rubens Peculis <rubens.peculis@mdba.gov.au>
2013-07-29 09:41:01 +1000

    Added hide code to category stylesheet

--- css/custom_category_css.php
+++ css/custom_category_css.php
@@ -10,18 +10,27 @@ foreach($_GET as $key => $color) {
 $css = '';
 
 if(isset($values)) {
-  $css .= 
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-1,'."\n".
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-3,'."\n".
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-5 {background: '. $values['oddcolor'] .';}'."\n";
+  if(isset($values['oddcolor'])){
+    $css .= 
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-1,'."\n".
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-3,'."\n".
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-5 {background: '. $values['oddcolor'] .';}'."\n";
+  }
+  if(isset($values['evencolor'])){
+    $css .=
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-2,'."\n".
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-4,'."\n".
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-6 {background: '. $values['evencolor'] .';}'."\n";
+  }
 
-  $css .=
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-2,'."\n".
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-4,'."\n".
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag-row-6 {background: '. $values['evencolor'] .';}'."\n";
-
-  $css .=
-    '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag div h3 {color: '. $values['catfontcolor'] .';}';
+  if(isset($values['catfontcolor'])){
+    $css .=
+      '.block-timelinejs-block .vco-timeline .vco-navigation .timenav-background .timenav-tag div h3 {color: '. $values['catfontcolor'] .';}'."\n";
+  }
+  if(isset($values['catDisplay']) && $values['catDisplay'] == 'hide'){
+    $css .= '.timenav-tag h3 { display: none !Important; }'."\n";
+    $css .= '.slide-tag { display: none; }'."\n";
+  }
 }
 
 echo $css;

2a89e36dfa5e377b6a489781a315ffeba2169425
Rubens Peculis <rubens.peculis@mdba.gov.au>
2013-08-29 18:05:24 +1000

    Working prototype

--- scripts/timeline_mod.js
+++ scripts/timeline_mod.js
@@ -12,36 +12,44 @@
         scrollTop: $('.timelinejs_table').offset().top
         }, 'fast');
     });
-  });
 
-  $(window).load(function(){
-
-    $('#storyjs-timeline').on('LOADED', function(event){
-      window.location = '#2';
-      window.location = '#1';
+    $(document).on('keydown', '.storyjs-embed', function(e){
+      aa();
     });
+    $(document).on('click', '.flag', aa);
 
-    $('#storyjs-timeline').on('UPDATE', function(event){
-
+    $(document).on('UPDATE', '#storyjs-timeline', function(event) {
       var target = event.target;
       if($(target).hasClass('vco-slider')){
-
-        $('.slider-item-container').children('div.slider-item').each(function(){
-            var date = $(this).find($('h2.date')).html();
-            var headline = '<span class="capitalize">' + $(this).find($('h3')).html() + '</span>';
-            if(!!date && !!headline){
-              var newHeadline = '<div class="headline">'+ headline + ' - ' + date  +'</div>';
-              var check = $(this).find($('div.headline'));
-              if(!!check){
-                $(this).find($('.headline')).remove();
-                $(this).prepend(newHeadline);
-              }
-            }
-          });
-
+        aa();
       }
     });
+
+    $(document).on('LOADED', '#storyjs-timeline', function(event){
+      var target = event.target;
+      if($(target).hasClass('vco-navigation')){
+        var newHeadline = '<div class="headline"><span class="capitalize">Basin Plan comes into effect</span> - November 2012 </div>';
+        $('.slider-item-container div.slider-item:nth-child(2)').prepend(newHeadline);
+        $('.content .marker:nth-child(2) .flag').trigger('click');
+      }
+
+    });
   });
+
+  function aa(){
+    $('.slider-item-container').children('div.slider-item').each(function(){
+      var date = $(this).find($('h2.date')).html();
+      var headline = '<span class="capitalize">' + $(this).find($('h3')).html() + '</span>';
+      if(!!date && !!headline){
+        var newHeadline = '<div class="headline">'+ headline + ' - ' + date  +'</div>';
+        var check = $(this).find($('div.headline'));
+        if(!!check){
+          $(this).find($('.headline')).remove();
+          $(this).prepend(newHeadline);
+        }
+      } 
+    });
+  }
 
 })(jQuery);
 
